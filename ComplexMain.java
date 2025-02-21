214725b0879a8ad35018fdbc14ec09c31023aa60
Aiswaryapdas <146425097+Aiswaryapdas@users.noreply.github.com>
2025-02-21 15:46:20 +0530

    Add files via upload

---
+++ ComplexMain.java
@@ -0,0 +1,43 @@
+class Complex{
+	double real, imag;
+
+	public void setComplexNumber(double realPart, double imagPart){
+		real = realPart;
+		imag = imagPart;
+	}
+
+	public void display(){
+		if(imag < 0 )
+			System.out.println(real+"-"+Math.abs(imag)+"i");
+		else
+			System.out.println(real +"+"+ imag + "i");
+		}
+
+	public Complex add(Complex c){
+		double resultReal = real + c.real;
+		double resultImag = imag + c.imag;
+		Complex sum= new Complex();
+		sum.setComplexNumber(resultReal,resultImag);
+		return sum;
+		}
+	}
+
+	public class ComplexMain{
+		public static void main(String args[]){
+			Complex complex1 = new Complex();
+			complex1.setComplexNumber(3.5,4.5);
+
+			Complex complex2 = new Complex();
+			complex2.setComplexNumber(2.5,.5);
+
+			System.out.println("Complex Number 1:");
+			complex1.display();
+			System.out.println("Complex Number 2:");
+			complex2.display();
+
+			Complex sum= complex1.add(complex2);
+
+			System.out.print("Sum of the complex numbers:");
+			sum.display();
+		}
+	}
---
+++ Main.java
@@ -0,0 +1,50 @@
+class Product{
+	String pcode;
+	String pname;
+	double price;
+	
+	public void setProductDetails(String productCode, String productName, double productPrice){
+		pcode= productCode;
+		pname= productName;
+		price= productPrice;
+		}
+	public double getPrice(){
+		return price;
+		}
+
+	public void displayProduct(){
+		System.out.println("Product Code:" +pcode);
+		System.out.println("ProductName:" +pname);
+		System.out.println("ProdcutPrice:" +price);
+		}
+	}
+public class Main{
+	public static void main(String Args[]){
+		Product product1= new Product();
+		product1.setProductDetails("P001","Product1",100.0);
+		
+		Product product2= new Product();
+		product2.setProductDetails("P002","Product2",50.0);
+		
+		Product product3= new Product();
+		product3.setProductDetails("P003","Product3",150.0);
+
+		Product lowestPriceProduct=product1;
+		
+		if(product2.getPrice()<lowestPriceProduct.getPrice()){
+			lowestPriceProduct=product2;
+		}
+		if(product3.getPrice()<lowestPriceProduct.getPrice()){
+			lowestPriceProduct=product3;
+		}
+
+		System.out.println("\nProduct with the lowest price:");
+		lowestPriceProduct.displayProduct();
+		}
+	}
+
+
+
+
+
+
