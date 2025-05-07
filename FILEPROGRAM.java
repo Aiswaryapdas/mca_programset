8a1ca8340a58f5dd938635e3e4e2d121e2e6f7f2
Aiswaryapdas <146425097+Aiswaryapdas@users.noreply.github.com>
2025-05-07 11:40:14 +0530

    Add files via upload

---
+++ FILEPROGRAM.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FILEPROGRAM {
+	public static void main(String args[]) {
+		try {
+			FileWriter writer = new FileWriter("fp.txt", true);
+			writer.write("THIS MY FIRST FILE PROGRAM YOU ARE READING FROM THE FILE CREATED");
+			writer.close();
+			FileReader reader = new FileReader("fp.txt");
+			BufferedReader br = new BufferedReader(reader);
+			String line;
+			System.out.println("Data read from the file");
+			while (( line = br.readLine() ) != null) {
+				System.out.println(line);
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("-------EXCEPTION Error-------");
+		}
+	}
+}
+		
